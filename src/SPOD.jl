f396a21688ac05f9142852d9555cae7311038a1c
Thomas Burton <tburton5572@gmail.com>
2023-03-27 12:55:53 +0100

    more generic (still needs testing)

--- src/SPOD.jl
+++ src/SPOD.jl
@@ -35,53 +35,21 @@ for window in window_types
 end
 
 
-snap2field!(u, q) = (u[:] = q[:]; return u)
-field2snap(u) = u[:]
-
-"""
-    Take a set of DNS data and convert it to a snapshot matrix.
-"""
-function construct_snapshot_matrix(loc::String); end
-
-"""
-    Fourier transform the snapshot matrix in the homogeneous directions, and
-    return the result as a similar snapshot matrix.
-"""
-function fft_homogeneous_directions(Q::Matrix, S::NTuple{3, Int})
-    # initialise useful arrays
-    Q_new = zeros(ComplexF64, prod((S[1], (S[2] >> 1) + 1, S[3])), size(Q, 2))
-    u = zeros(Float64, S...)
-
-    # loop over time dimensions
-    for ti in 1:size(Q, 2)
-        # convert snapshot vector to field array
-        snap2field!(u, @view(Q[:, ti]))
-
-        # fourier transform the data in streamwise and spanwise directions
-        # TODO: does this need normalisation?
-        û = FFTW.rfft(u, [1, 3])
-
-        # convert back into snapshot vector for spectral space and assign to new snapshot matrix
-        Q_new[:, ti] = field2snap(û)
-    end
-
-    return Q_new
-end
-
 """
     Split the (Fourier transformed) snapshot matrix into Nf blocks in the time
     direction, with a specified overlap.
 """
-function split_into_blocks(Q::Matrix, Nf::Int, No::Int)
+function split_into_blocks(Q::M, Nf::Int, No::Int) where {M <: AbstractMatrix}
     # compute the number of blocks
-    Nb = Int(floor((size(Q, 2) - No)/(Nf - No)))
+    Nb = floor(Int, (size(Q, 2) - No)/(Nf - No))
 
     # initialise vector to hold block arrays
-    Q_blocks = [Matrix{ComplexF64}(undef, size(Q, 1), Nf) for _ in Nb]
+    Q_blocks = [M(undef, size(Q, 1), Nf) for _ in Nb]
 
     # loop over the blocks and take views of snapshot matrix
     for nb in 1:Nb
         block_index_dist = (nb - 1)*(Nf - No)
+        # TODO: change to copyto! so that the original matrix isn't affected by windowing
         Q_blocks[nb] = @view(Q[:, (1 + block_index_dist):(Nf + block_index_dist)])
     end
 
@@ -92,7 +60,7 @@ end
 """
     Compute the Fourier transform of a single block of a total snapshot matrix. 
 """
-fft_time(Q::Matrix) = FFTW.fft(Q, 2)
+fft_time(Q::AbstractMatrix) = FFTW.fft(Q, 2)
 
 """
     spod(
@@ -108,7 +76,7 @@ fft_time(Q::Matrix) = FFTW.fft(Q, 2)
     currently supported. The decomposition can be truncated by passing a unit
     range to eigrange.
 """
-function spod(Q::Matrix{ComplexF64}, quad_weights::Vector{Float64}, Nf::Int, No::Int=0; window::WindowMethod=NoWindow() eigrange::Union{Nothing, UnitRange}=nothing)
+function spod(Q::M, quad_weights::AbstractVector, Nf::Int, No::Int=0; window::WindowMethod=NoWindow(), eigrange::Union{Nothing, UnitRange}=nothing) where {M <: AbstractMatrix}
     # get size of snapshot vectors
     N = size(Q, 1)
 
@@ -117,7 +85,7 @@ function spod(Q::Matrix{ComplexF64}, quad_weights::Vector{Float64}, Nf::Int, No:
     Nb = length(Q_blocks)
 
     # initialise matrix to hold FFT of snapshot matrix blocks
-    Q̂_blocks = Vector{Matrix{ComplexF64}}(undef, length(Q_blocks))
+    Q̂_blocks = Vector{typeof(Q)}(undef, length(Q_blocks))
     Nω = size(Q̂_blocks[1], 2)
 
     # loop over blocks and perform the time-wise FFT
@@ -126,12 +94,9 @@ function spod(Q::Matrix{ComplexF64}, quad_weights::Vector{Float64}, Nf::Int, No:
     end
 
     # initialise useful arrays
-    Qfk = zeros(ComplexF64, N, Nb)
-    Mfk = zeros(ComplexF64, Nb, Nb)
-    spod_modes = [Matrix{ComplexF64}(undef, N, Nb) for _ in 1:Nω]
-
-    # # construct the weight matrix (quadrature + windowing)
-    W = diag(quad_weights)
+    Qfk = M(undef, N, Nb)
+    Mfk = M(undef, N, Nb)
+    spod_modes = [M(undef, N, Nb) for _ in 1:Nω]
 
     # loop over the frequencies of all the blocks
     for fk in 1:Nω
@@ -141,7 +106,7 @@ function spod(Q::Matrix{ComplexF64}, quad_weights::Vector{Float64}, Nf::Int, No:
         end
 
         # compute the weights cross-spectral density matrix for each frequency
-        Mfk .= adjoint(Qfk)*W*Qfk
+        Mfk .= adjoint(Qfk)*Diagonal(quad_weights)*Qfk
 
         # compute the eigenvalue decomposition
         eigvals, eigvecs = eigen(Hermitian(Mfk), sortby=(x -> -real(x)))
@@ -156,6 +121,6 @@ function spod(Q::Matrix{ComplexF64}, quad_weights::Vector{Float64}, Nf::Int, No:
     return eigvals, spod_modes
 end
 
-truncate_eigen!(::Vector{T}, ::Matrix{Complex{T}}, ::Nothing) where {T} = nothing
-truncate_eigen!(eigvals::Vector{T}, eigvecs::Matrix{Complex{T}}, eigrange::UnitRange) where {T} = (eigvals[eigrange]; eigvecs[:, eigrange]; return nothing)
+truncate_eigen!(::AbstractVector, ::AbstractMatrix, ::Nothing) = nothing
+truncate_eigen!(eigvals::AbstractVector, eigvecs::AbstractMatrix, eigrange::UnitRange) = (eigvals[eigrange]; eigvecs[:, eigrange]; return nothing)
 end
